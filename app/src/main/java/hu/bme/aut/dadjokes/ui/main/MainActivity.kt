7f83dc8be82d6caa1c6e0fc750cc666004134382
Kristof Kalai <kristof98@gmail.com>
2022-04-28 12:54:15 +0200

    feat: add google analytics and crashlytics

--- app/src/main/java/hu/bme/aut/dadjokes/ui/main/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/dadjokes/ui/main/MainActivity.kt
@@ -1,16 +1,31 @@
 package hu.bme.aut.dadjokes.ui.main
 
+import android.R.id
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import hu.bme.aut.dadjokes.ui.Screen
 
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        firebaseAnalytics = Firebase.analytics
+
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "MainActivity")
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "MainActivity")
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "view")
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM, bundle)
 
         setContent {
             MaterialTheme {
